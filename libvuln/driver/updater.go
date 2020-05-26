db4f619ba03bf7d2717639b56e501015e3ab99a6
Hank Donnay <hdonnay@redhat.com>
2020-05-26 12:18:14 -0400

    driver, indexer: add Configurable interfaces
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- libvuln/driver/updater.go
+++ libvuln/driver/updater.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"io"
+	"net/http"
 
 	"github.com/quay/claircore"
 )
@@ -18,8 +19,9 @@ type Updater interface {
 
 // Parser is an interface which is embedded into the Updater interface.
 //
-// Parse should be called with an io.ReadCloser struct where the contents of a security
-// advisory databse can be read and parsed into an array of *claircore.Vulnerability
+// Parse should be called with an io.ReadCloser where the contents of a security
+// advisory database can be read and parsed into a slice of
+// *claircore.Vulnerability.
 type Parser interface {
 	// Parse should take an io.ReadCloser, read the contents, parse the contents
 	// into a list of claircore.Vulnerability structs and then return
@@ -29,13 +31,15 @@ type Parser interface {
 
 // Fetcher is an interface which is embedded into the Updater interface.
 //
-// When called the interface should determine if new security advisory data is available.
-// Fingerprint may be passed into in order for the Fetcher to determine if the contents has changed
+// When called, the method should determine if new security advisory data is
+// available. A Fingerprint is provided in order for the Fetcher to determine if
+// the contents have changed.
 //
-// If there is new content Fetcher should return a io.ReadCloser where the new content can be read.
-// Optionally a fingerprint can be returned which uniqeually identifies the new content.
+// If there is new content, Fetch should return an io.ReadCloser where the new
+// content can be read.  A Fingerprint that uniquely identifies the new content
+// should be returned as well.
 //
-// If the conent has not change an  Unchanged error should be returned.
+// If the content has not changed, an Unchanged error should be returned.
 type Fetcher interface {
 	Fetch(context.Context, Fingerprint) (io.ReadCloser, Fingerprint, error)
 }
@@ -43,5 +47,16 @@ type Fetcher interface {
 // Unchanged is returned by Fetchers when the database has not changed.
 var Unchanged = errors.New("database contents unchanged")
 
-// Fingerprint is some identifiying information about a vulnerability database.
+// Fingerprint is some identifying information about a vulnerability database.
 type Fingerprint string
+
+// ConfigUnmarshaler can be thought of as a curried Unmarshal function, or a
+// Decode function. The function should populate a passed struct with any
+// configuration information.
+type ConfigUnmarshaler func(interface{}) error
+
+// Configurable is an interface that Updaters can implement to opt-in to having
+// their configuration provided dynamically.
+type Configurable interface {
+	Configure(context.Context, ConfigUnmarshaler, *http.Client) error
+}
